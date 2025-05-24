c4f73298dc586af73d275df9f2b0137b21973da8
shakya <152844822+kyashark@users.noreply.github.com>
2025-05-24 17:09:11 +0530

    Create Task routes

--- backend/Server.js
+++ backend/Server.js
@@ -4,6 +4,8 @@ const dotenv = require('dotenv');
 const cors = require('cors');
 const connectDB = require('./config/db');
 
+const taskRoutes = require('./routes/TaskRoutes');
+
 dotenv.config();
 connectDB();
 
@@ -11,7 +13,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+
 app.get('/', (req, res) => res.send('API is running...'));
+app.use('/api/tasks', taskRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+
+
+
