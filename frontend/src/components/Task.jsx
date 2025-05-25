35c272dd81158d23a057c7e82ae6453c24b4891a
shakya <152844822+kyashark@users.noreply.github.com>
2025-05-25 11:12:31 +0530

    Fetch all tasks

--- frontend/src/components/Task.jsx
+++ frontend/src/components/Task.jsx
@@ -1,39 +1,47 @@
-import TaskStyle from './Task.module.css'
+import { useEffect, useState } from "react";
+import TaskStyle from "./Task.module.css";
+import axios from "axios";
 
-const Task = ()=>{
-    return(
-        <>
-        <div className={TaskStyle.container}>
-      <form className={TaskStyle.addTask}>
-        <input type="text"></input>
-        <button type="submit">Add</button>
-      </form>
-      <div className={TaskStyle.taskList}>
-      <div className={TaskStyle.task}>
-        <input type="checkbox" className={TaskStyle.checkbox}></input>
-        <span>Lorem ipsum dolor</span>
+const Task = () => {
+
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(()=>{
+    axios
+    .get("http://localhost:5000/api/tasks")
+    .then((response) =>{
+      setTasks(response.data);
+    })
+    .catch((error) => {
+      console.error(
+        "There was an error fetching the tasks data",
+        error
+      );
+    });
+  },[]);
+
+  return (
+    <>
+      <div className={TaskStyle.container}>
+        <form className={TaskStyle.addTask}>
+          <input type="text"></input>
+          <button type="submit">Add</button>
+        </form>
+        <div className={TaskStyle.taskList}>
+          {/* <div className={TaskStyle.task}>
+            <input type="checkbox" className={TaskStyle.checkbox}></input>
+            <span>Lorem ipsum dolor</span>
+          </div> */}
+         {tasks.map(task =>(
+          <div className={TaskStyle.task} key={task._id}>
+            <input type="checkbox" className={TaskStyle.checkbox} checked={task.completed}></input>
+            <span>{task.title}</span>
+          </div>
+         ))}
+        </div>
       </div>
-      <div className={TaskStyle.task}>
-        <input type="checkbox" className={TaskStyle.checkbox}></input>
-        <span>Lorem ipsum dolor</span>
-      </div>
-      <div className={TaskStyle.task}>
-        <input type="checkbox" className={TaskStyle.checkbox}></input>
-        <span>Lorem ipsum dolor</span>
-      </div>
-      <div className={TaskStyle.task}>
-        <input type="checkbox" className={TaskStyle.checkbox}></input>
-        <span>Lorem ipsum dolor</span>
-      </div>
-      <div className={TaskStyle.task}>
-        <input type="checkbox" className={TaskStyle.checkbox}></input>
-        <span>Lorem ipsum dolor</span>
-      </div>
-      </div>
-    
-    </div>
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default Task;
